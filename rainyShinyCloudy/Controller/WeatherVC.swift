1019ebfffbb17d4f174bb5dabd1bee7fffaf92db
Shashank <shashankpanwar4@gmail.com>
2018-02-12 09:32:59 +0530

    Layout of wheather VC

---
+++ rainyShinyCloudy/Controller/WeatherVC.swift
@@ -0,0 +1,20 @@
+//
+//  weatherVC.swift
+//  rainyShinyCloudy
+//
+//  Created by Kirti Ahlawat on 11/02/18.
+//  Copyright © 2018 xyz. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
